e43e203e311c5c65e4a9c502a888fa28739033b5
Chris Osterwood <osterwood@gmail.com>
2018-09-21 07:26:26 -0400

    Fix encoding issue.  Change location of some keys in JSON settings.  Move to threading model

--- software/main.py
+++ software/main.py
@@ -4,6 +4,7 @@ import time
 import machine
 import network
 import ujson as json
+from    _thread     import start_new_thread
 
 ## Local Imports
 import adafruit_max31865 as max31865
@@ -11,10 +12,13 @@ import mqtt
 
 
 def check_wifi(nic):
+    if 'wifi-station' not in settings:
+        return
+
     if nic.isconnected():
         return
 
-    nic.connect(settings['wifi']['name'], settings['wifi']['pass'])
+    nic.connect(settings['wifi-station']['name'], settings['wifi-station']['pass'])
 
     while not nic.isconnected():
         time.sleep(0.2)
@@ -31,6 +35,63 @@ def stddev(data, ddof=0):
     ss = _ss(data)
     pvar = ss/(len(data)-ddof)
     return pvar**0.5
+
+def mqtt_thread():
+    ## Create MQTT Connection
+    c = mqtt.MQTTClient(settings['device']['name'], settings['mqtt']['ip'], 1883)
+    c.connect()
+
+    sensors     = []
+    sensor_info = []
+    idx         = 0
+
+    ## Create array of active RTD sensors and information about them
+    for location in settings['device']['sensors']:
+        idx += 1
+
+        if location.upper() == "DISABLE":
+            continue
+
+        sensors.append([
+            idx,
+            max31865.MAX31865(
+                spi, css[idx-1],
+                wires        = 4,
+                rtd_nominal  = RTD_NOMINAL,
+                ref_resistor = RTD_REFERENCE)
+        ])
+
+        sensor_info.append({
+            "id"       : idx,
+            "scale"    : settings['mqtt']['scale'],
+            "type"     : "temperature",
+            "sensor"   : "RTD1000 -> MAX31865",
+            "location" : location
+        })
+
+
+    c.publish(settings['device']['name']+'/', json.dumps(sensor_info))
+
+    while True:
+        check_wifi(nic)
+
+        meta    = []
+        samples = []
+
+        for idx, sensor in sensors:
+            value = sensor.temperature*settings['mqtt']['scale']
+
+            if settings['mqtt']['to_int']:
+                value = int(value)
+
+            meta.append({'id':idx, 'value' : value})
+            samples.append(value)
+
+        meta.append({'id':'agg','mean':mean(samples),'stddev':stddev(samples)})
+
+        c.publish(settings['device']['name']+'/', json.dumps(meta))
+        time.sleep(settings['mqtt']['sleep'])
+
 
 settings = json.load(open("settings.json", 'r'))
 
@@ -52,65 +113,14 @@ cs4  = machine.Pin(14, machine.Pin.OUT, value=1)
 css  = [cs1, cs2, cs3, cs4]
 
 ## Setup WIFI Networking
-nic = network.WLAN(network.STA_IF)
-nic.active(True)
-check_wifi(nic)
-
-## Create MQTT Connection
-c = mqtt.MQTTClient(settings['device']['name'], settings['mqtt']['ip'], 1883)
-c.connect()
-
-sensors     = []
-sensor_info = []
-idx         = 0
-
-## Create array of active RTD sensors and information about them
-for location in settings['device']['sensors']:
-    idx += 1
-
-    if location.upper() == "DISABLE":
-        continue
-
-    sensors.append([
-        idx,
-        max31865.MAX31865(
-            spi, css[idx-1],
-            wires        = 4,
-            rtd_nominal  = RTD_NOMINAL,
-            ref_resistor = RTD_REFERENCE)
-    ])
-
-    sensor_info.append({
-        "id"       : idx,
-        "scale"    : settings['device']['scale'],
-        "type"     : "temperature",
-        "sensor"   : "RTD1000 -> MAX31865",
-        "location" : location
-    })
-
-
-c.publish(settings['device']['name']+'/', json.dumps(sensor_info))
-
-while True:
+if 'wifi-station' in settings:
+    nic = network.WLAN(network.STA_IF)
+    nic.active(True)
     check_wifi(nic)
 
-    meta    = []
-    samples = []
-
-    for idx, sensor in sensors:
-        value = sensor.temperature*settings['device']['scale']
-
-        if settings['device']['to_int']:
-            value = int(value)
-
-        meta.append({'id':idx, 'value' : value})
-        samples.append(value)
-
-    meta.append({'id':'agg','mean':mean(samples),'stddev':stddev(samples)})
-
-    c.publish(settings['device']['name']+'/', json.dumps(meta))
-    time.sleep(settings['device']['sleep'])
-
+## Become a MQTT client, if configured so
+if 'mqtt' in settings:
+    start_new_thread(mqtt_thread, ())
 
 
 
